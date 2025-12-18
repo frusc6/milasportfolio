27679d9fe2ac8612fbda60fdaaf7ee21ddc7a2c0
frusc6 <markovicmiiila@gmail.com>
2025-12-18 23:17:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,15 @@ closeBtn.addEventListener("click", () => {
 modal.addEventListener("click", (e) => {
   if (e.target === modal) modal.classList.remove("active");
 });
+
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
